83145dea0b0bccaa9fd11fb2913c60a1d6d106f5
Hernan Y.Ke <yuriqiao@gmail.com>
2015-12-07 21:00:06 -0500

    Add login and logout

--- pkdjbyexampj2/settings.py
+++ pkdjbyexampj2/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from django.core.urlresolvers import reverse_lazy
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -26,6 +27,10 @@ SECRET_KEY = 'h@(gw1q@z2en%_*_e2kz_dg%ki^#fr(2%-jax30jbf&h4xmzbg'
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+LOGIN_REDIRECT_URL = reverse_lazy('dashboard')
+LOGIN_URL = reverse_lazy('login')
+LOGOUT_URL = reverse_lazy('logout')
 
 
 # Application definition
