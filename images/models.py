9b4b02e4a3fb33a1002a1d14194dfa4604a40896
Hernan Y.Ke <yuriqiao@gmail.com>
2015-12-10 11:57:30 -0500

    custom decorator

--- images/models.py
+++ images/models.py
@@ -11,7 +11,7 @@ class Image(models.Model):
     image = models.ImageField(upload_to='image/%Y/%m/%d')
     description = models.TextField(blank=True)
     created = models.DateField(auto_now_add=True,db_index=True)
-    user_like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='images_liked',blank=True)
+    users_like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='images_liked',blank=True)
 
     def save(self, *args, **kwargs):
         if not self.slug:
--- images/urls.py
+++ images/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^create/$', views.image_create,name='create'),
-    url(r'^detail/(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.image_create,name='create'),
+    url(r'^like/$', views.image_like,name='like'),
+    url(r'^detail/(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.image_create,name='detail'),
 ]
